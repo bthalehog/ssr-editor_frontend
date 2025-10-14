75d37dd0156dff207c342df5fcad1484af897db5
Alexander Högfeldt <alhf24@student.bth.se>
2025-10-14 21:07:08 +0200

    TEsting npm audit force fix

--- .svelte-kit/output/client/_app/immutable/entry/start.B0MFkuJK.js
+++
@@ -1,1 +0,0 @@
-import{l as o,a as r}from"../chunks/BtPb0BhN.js";export{o as load_css,r as start};
--- .svelte-kit/output/server/nodes/4.js
+++ .svelte-kit/output/server/nodes/4.js
@@ -3,6 +3,6 @@
 export const index = 4;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/documents/_id_/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/4.Dyv6_8jW.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CFzMRsDo.js","_app/immutable/chunks/DZm4wIEF.js","_app/immutable/chunks/C--EXBPb.js","_app/immutable/chunks/DJNdyps-.js","_app/immutable/chunks/DkvNNDwX.js","_app/immutable/chunks/vD9QUtaX.js","_app/immutable/chunks/Dbw1WY8o.js","_app/immutable/chunks/DYK-QI13.js","_app/immutable/chunks/B4KX5r6Q.js","_app/immutable/chunks/DFrMXiLK.js","_app/immutable/chunks/Bdp2eOd4.js","_app/immutable/chunks/BtPb0BhN.js"];
+export const imports = ["_app/immutable/nodes/4.BkdF_U-A.js","_app/immutable/chunks/DsnmJJEf.js","_app/immutable/chunks/CFzMRsDo.js","_app/immutable/chunks/DZm4wIEF.js","_app/immutable/chunks/C--EXBPb.js","_app/immutable/chunks/DJNdyps-.js","_app/immutable/chunks/DkvNNDwX.js","_app/immutable/chunks/vD9QUtaX.js","_app/immutable/chunks/Dbw1WY8o.js","_app/immutable/chunks/DYK-QI13.js","_app/immutable/chunks/B4KX5r6Q.js","_app/immutable/chunks/DFrMXiLK.js","_app/immutable/chunks/Bdp2eOd4.js","_app/immutable/chunks/WIY2h3HN.js"];
 export const stylesheets = ["_app/immutable/assets/Footer.CmYLLcsZ.css"];
 export const fonts = [];
