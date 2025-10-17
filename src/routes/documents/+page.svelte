aaeb7db4453391115e835ed125858b20b3df4171
Alexander Högfeldt <alhf24@student.bth.se>
2025-10-17 11:49:50 +0200

    Updated documents svelte route

--- src/routes/documents/+page.svelte
+++ src/routes/documents/+page.svelte
@@ -12,7 +12,7 @@
 
     onMount(async () => {        
         try {
-            const response = await fetch(`${API_BASE}/api/documents`);
+            const response = await fetch(`${API_BASE}/api`); // NOT /documents
             const data = await response.json();
             documents = data.docs;
         } catch (err) {
