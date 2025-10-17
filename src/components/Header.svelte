502d4c092df496dcc0d2e20b67232e3190cd7244
Alexander Högfeldt <alhf24@student.bth.se>
2025-10-17 07:20:32 +0200

    Debug route

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,8 +5,8 @@
 <header>
     <h1>SSR Editor</h1>
     <navigation>
-        <a href="/documents" on:click|preventDefault={() => goto('/documents')}>Start</a>
-        <a href="/documents/list" on:click|preventDefault={() => goto('/documents/list')}>Lista</a>
-        <a href="/documents/create" on:click|preventDefault={() => goto('/documents/create')}>Nytt dokument</a>
+        <a href="/documents" on:click|preventDefault={() => goto('ssr-editor_frontend/documents')}>Start</a>
+        <a href="/documents/list" on:click|preventDefault={() => goto('ssr-editor_frontend/documents/list')}>Lista</a>
+        <a href="/documents/create" on:click|preventDefault={() => goto('ssr-editor_frontend/documents/create')}>Nytt dokument</a>
     </navigation>
 </header>
