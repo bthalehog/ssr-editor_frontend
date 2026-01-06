b1d158851c35289b856028902a04b40bdd889bde
Alexander Högfeldt <alhf24@student.bth.se>
2026-01-06 21:05:00 +0100

    Base again, build with jekyll

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -91,11 +91,11 @@
     </form>
 
     <p class="auth-link">
-        Har du inget konto? <a href="https://bthalehog.github.io/ssr-editor_frontend/register">Registrera dig</a>!
+        Har du inget konto? <a href="{base}/register">Registrera dig</a>!
     </p>
 
     <p class="auth-link">
-        Du kan läsa <a href="https://bthalehog.github.io/ssr-editor_frontend/register/documents">dokumenten i databasen</a> som gäst.
+        Du kan läsa <a href="{base}/documents">dokumenten i databasen</a> som gäst.
     </p>
     
 </main>
